1dd4a34f5d860224a4f5fc062b41bc52045f3e20
jackson18 <qijiabin18@126.com>
2016-06-21 10:23:12 +0800

    优化

--- src/main/java/com/qijiabin/core/common/Constant.java
+++ src/main/java/com/qijiabin/core/common/Constant.java
@@ -12,8 +12,8 @@ package com.qijiabin.core.common;
  */
 public interface Constant {
 
-    int ZK_SESSION_TIMEOUT = 5000;
-    String ZK_REGISTRY_PATH = "/registry";
-    String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
+    public static final int ZK_SESSION_TIMEOUT = 5000;
+    public static final String ZK_REGISTRY_PATH = "/registry";
+    public static final String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
     
 }
