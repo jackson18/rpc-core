f187a5e46e746970f6b8fe31cd6f38038f2dba99
jackson18 <qijiabin18@126.com>
2016-06-27 15:24:03 +0800

    优化

---
+++ src/main/java/com/qijiabin/core/proxy/support/JdkProxyFactory.java
@@ -0,0 +1,27 @@
+package com.qijiabin.core.proxy.support;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+import com.qijiabin.core.proxy.ProxyFactory;
+
+/**
+ * ========================================================
+ * 日 期：2016年6月27日 下午2:23:57
+ * 作 者：qijiabin
+ * 版 本：1.0.0
+ * 类说明：
+ * TODO
+ * ========================================================
+ * 修订日期     修订人    描述
+ */
+public class JdkProxyFactory implements ProxyFactory {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T getProxy(Class<T> clz, InvocationHandler invocationHandler) {
+		return (T) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[] {clz}, invocationHandler);
+	}
+
+}
+
